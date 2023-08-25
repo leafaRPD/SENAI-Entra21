af0876cc3e62788dcd10ce8c2579e9a738fb4a98
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-08-24 22:12:44 -0300

    Atualização fim de aula

--- Aula 9 - 2023-08-24/Atividade - Estrut. de repetição (while e do while).js
+++ Aula 9 - 2023-08-24/Atividade - Estrut. de repetição (while e do while).js
@@ -32,7 +32,7 @@ while(popA < popB) {
 // 2) Os números primos possuem várias aplicações dentro da Computação, por exemplo, na criptografia. Um número primo é aquele que é divisível apenas por um e por ele mesmo. Faça um programa que peça ao usuário para digitar um número e informe se ele é primo. Depois implemente um laço de repetição para solicitar cinco números inteiros e mostre na tela se são primos ou não.
 
 
-let numUm, numDois, numTres, numQuatro, numCinco
+let numUm //, numDois, numTres, numQuatro, numCinco
 let i = 3
 // alert(`Insira números para verificar se são primos.`)
 numUm = Number(prompt(`Insira o primeiro número a ser verificado:`))
@@ -54,9 +54,6 @@ console.log(`${numUm} é primo.`)
 console.log(`${numUm} não é primo.`)
 }}
 
-
-
-
 // 3) Faça um programa que gera uma lista dos números primos existentes entre 1 e um número inteiro digitado pelo usuário.
 
 let num // ex: 28
@@ -65,8 +62,6 @@ let lista
 numUm = Number(prompt(`Insira o primeiro número a ser verificado:`))
 
 while (num%i != 0) {
-
-
 }
 
 // 4) Você é um colecionador de vinis e resolveu ir ao sebo para comprar alguns vinis raros. Porém, você tem apenas R$200 para gastar. Faça um programa que pergunte o nome do vinil e o valor, repetidamente. Caso o valor ultrapasse R$200, encerrar (desconsiderando o último vinil). Mostrar na tela:
@@ -79,10 +74,64 @@ while (num%i != 0) {
 
 // 5) Santa Catarina é conhecida por suas quatro estações bem definidas, o que o torna um estado com alta amplitude térmica (diferença entre a maior e a menor temperatura, registrada em um período). O INMET, Instituto Nacional de Meteorologia, pretende ter os dados de amplitude térmica de uma cidade ou estado. Desenvolva um programa que calcule e mostre os dados diários (segunda à sexta) e a média de amplitude térmica semanal, mostrando os resultados ao final. Exemplo: Digitar o nome da cidade ou estado, perguntar para os cinco dias da semana a maior temperatura do dia e a menor. Mostrar as amplitudes de cada dia e a média.
 
+// variáveis: 
+let estado
+let diaSemana = 0;
+let ampTerm = 0; // amplitude térmica
+let mediaAmpTerm = 0
+
+do {
+    let maiorTemp = Number(prompt(`Qual a maior temperatura do dia?`))
+    let menorTemp = Number(prompt(`Qual a menor temperatura do dia?`))
+    ampTerm = maiorTemp - menorTemp;
+    alert(`A amplitude térmica de hoje é ${ampTerm}.`)
+    mediaAmpTerm += ampTerm
+
+    diaSemana++
+}while (diaSemana<5)
+
+mediaAmpTerm = mediaAmpTerm/5
+console.log(`A média semanal da amplitude térmica é de ${mediaAmpTerm}`)
+
+
 // 6) SCRUM é uma metodologia ágil de gerenciamento de projetos muito utilizada na área da tecnologia. Dentro do SCRUM, o planejamento é realizado através de 'sprints' que normalmente variam de 15 a 30 dias. Criar um programa onde o usuário deve digitar o tempo total do projeto em dias e o período das sprints. Após calculado o número de sprints (média), arredondando o número para baixo, cadastrar para cada sprint um título e um objetivo.
 
 // 7) Em jogos de tabuleiro do gênero RPG, um dos dados que pode ser utilizado é o dado D20. Esse dado possui 20 faces ou "lados" com valores de 1 a 20. Criar um sistema simples de jogo, onde no início é solicitado o número de jogadores e para cada jogador deve-se armazenar valores de quatro jogadas de um D20. A pontuação do jogador é definida excluindo o primeiro e o último valor, somando então os valores da segunda e terceira jogadas. Ao final mostrar a soma de cada jogador e suas posições. (DESAFIO: validar os valores de 1 a 20, ou seja, caso um valor digitado não esteja entre dentro dos valores do dado, repetir a pergunta)
 
+// utilizou a ideia de " jogador+i "
+
+let numeroJogadores
+let i = 0
+numeroJogadores = Number(prompt("Digite o número de jogadores:"))
+
+while(i<numeroJogadores) {
+    let jogadorNome = "Jogador "+(1+i); // Aqui foi o pulo do gato: inserir o i dentro de uma variável declarada dentro do while. É um while dentro do outro.
+    let somaPontuação = 0;
+    let quantidadeDeJogadas = 0;
+    
+    while (quantidadeDeJogadas < 4){ // porque são 4 jogadas
+        let dado = Number(prompt(`Insira o valor obtido no dado para o ${jogadorNome}.`));
+    
+        while(dado > 20 || dado < 0 ) {
+            dado = Number(prompt(`Valor inválido. Insira novamente o valor obtido no dado para o ${jogadorNome}.`))
+        }
+        quantidadeDeJogadas++
+        if (quantidadeDeJogadas === 2 || quantidadeDeJogadas === 3) {
+            somaPontuação += dado;
+        }
+    }
+
+    console.log(`${jogadorNome} - Pontuação: ${somaPontuação}.`);
+
+    i++
+}
+
+
+
+
+
+
 // 8) Na maioria das linguagens de programação, é possível contar caracteres de uma string (palavra, frase, etc). Criar um sistema no qual o usuário digite 5 palavras e ao final mostre o tamanho em número de caracteres, de cada uma delas. (Pesquisar sobre o comando length, para solução).
 
 // 9) Um determinado restaurante trabalha com limite de lotação de 50 pessoas. Porém, mesmo com esse limite, o restaurante pode encerrar seu atendimento antes do horário por falta de insumos para produção das refeições. Criar um programa que receba os dados da quantidade de pessoas que chegam ao restaurante (repetidamente) e se os insumos estão "ok" ou "insuficientes". Caso alguma das duas possibilidades aconteça, encerrar o programa e mostrar ao final o motivo do encerramento (atingiu limite de pessoa ou faltaram insumos).
+
