e2f68865324802c7023d5c38a2d744237602bf35
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-08-29 11:18:58 -0300

    Atualizações dos exercícios

--- Aula 7 - 2023-08-22/Lista de exercícios/Atividade - Estruturas Condicionais (SWITCH-CASE).js
+++ Aula 7 - 2023-08-22/Lista de exercícios/Atividade - Estruturas Condicionais (SWITCH-CASE).js
@@ -1,4 +1,4 @@
-                                                                    // Atividade - Estruturas Condicionais (SWITCH / CASE)
+																	// Atividade - Estruturas Condicionais (SWITCH / CASE)
 // 1) Fazer um programa onde o usuário pode acessar as seguintes opções:
 
 // A) Falar com a atendente    B) Falar com o RH
@@ -10,29 +10,29 @@ let opcaoMenu
 opcaoMenu = prompt(`Digite uma das opções: \n A- Falar com a atendente \n B- Falar com o RH \n C- Falar com o gerente \n D- Sair`).toUpperCase()
 
 switch(opcaoMenu){
-    case 'A':
+	case 'A':
 
-        alert(`Você escolheu a opção ${opcaoMenu}: Falar com a atendente.`)
-        break
+		alert(`Você escolheu a opção ${opcaoMenu}: Falar com a atendente.`)
+		break
 
-    case 'B':
+	case 'B':
 
-        alert(`Você escolheu a opção ${opcaoMenu}: Falar com o RH.`)
-        break
+		alert(`Você escolheu a opção ${opcaoMenu}: Falar com o RH.`)
+		break
 
-    case 'C':
+	case 'C':
 
-        alert(`Você escolheu a opção ${opcaoMenu}: Falar com o gerente.`)
-        break
-    
-    case 'D':
+		alert(`Você escolheu a opção ${opcaoMenu}: Falar com o gerente.`)
+		break
+	
+	case 'D':
 
-        alert(`Você escolheu a opção ${opcaoMenu}: Sair.`)
-        break
+		alert(`Você escolheu a opção ${opcaoMenu}: Sair.`)
+		break
 
-    default:
+	default:
 
-        alert(`A opção ${opcaoMenu} é inválida.`)
+		alert(`A opção ${opcaoMenu} é inválida.`)
 
 }
 
@@ -46,25 +46,25 @@ switch(opcaoMenu){
 
 let anoNasc // ano de nascimento da pessoa
 anoNasc = Number(prompt(`Digite o ano de seu nascimento. Ex: 1981`))
-        switch(true){
-        case anoNasc <= 1945:
-            alert('Você não faz parte da uma "Geração"')
-            break
-        case anoNasc > 1945 && anoNasc <= 1964:
-            alert('Você faz parte da Geração dos Baby Boomers.')
-            break
-        case anoNasc > 1964 && anoNasc <= 1980:
-            alert('Você faz parte da Geração X.')
-            break
-        case anoNasc > 1980 && anoNasc <= 1996:
-            alert('Você faz parte da Geração Y (ou Millennials).')
-            break
-        case anoNasc > 1996 && anoNasc <= 2010:
-            alert('Você faz parte da Geração Z.')
-            break
-        case anoNasc > 2010:
-            alert('Você faz parte da Geração Alfa.')
-            break
+		switch(true){
+		case anoNasc <= 1945:
+			alert('Você não faz parte da uma "Geração"')
+			break
+		case anoNasc > 1945 && anoNasc <= 1964:
+			alert('Você faz parte da Geração dos Baby Boomers.')
+			break
+		case anoNasc > 1964 && anoNasc <= 1980:
+			alert('Você faz parte da Geração X.')
+			break
+		case anoNasc > 1980 && anoNasc <= 1996:
+			alert('Você faz parte da Geração Y (ou Millennials).')
+			break
+		case anoNasc > 1996 && anoNasc <= 2010:
+			alert('Você faz parte da Geração Z.')
+			break
+		case anoNasc > 2010:
+			alert('Você faz parte da Geração Alfa.')
+			break
 }
 
 // nesse exercício, o professor usou apenas 1 alert no final, e definiu a variável 'geracao' para cada 'case'. Se satisfizer a condição (se for 'true'), geracao = "Geração X".
@@ -76,31 +76,31 @@ let ano
 ano = Number(prompt(`Digite o número correspondente ao mês: \n 1 - Janeiro \n 2 - Fevereiro \n 3 - Março \n 4 - Abril \n 5 - Maio \n 6 - Junho \n 7 - Julho \n 8 - Agosto \n 9 - Setembro \n 10 - Outubro \n 11 - Novembro \n 12 - Dezembro`))
 
 switch(ano){
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
 
-        alert(`O mês ${ano} têm 31 dias.`)
-        break
+		alert(`O mês ${ano} têm 31 dias.`)
+		break
 
-    case 2:
-        alert(`O mês ${ano} têm 28 dias.`)
-        break
+	case 2:
+		alert(`O mês ${ano} têm 28 dias.`)
+		break
 
-    case 4:
-    case 6:
-    case 9:
-    case 11:
+	case 4:
+	case 6:
+	case 9:
+	case 11:
    
-         alert(`O mês ${ano} têm 30 dias.`)
-        break
-    default:
+		 alert(`O mês ${ano} têm 30 dias.`)
+		break
+	default:
 
-        alert(`A opção ${ano} é inválida.`)
+		alert(`A opção ${ano} é inválida.`)
 }
 
 // 4) Desenvolva um programa que pergunte ao usuário o número da sua conta bancária e o tipo de operação a ser realizada: 1) Saldo 2) Depósito 3) Saque. Nas opções de depósito e saque, perguntar o valor a ser depositado ou sacado e mostrar o saldo atualizado na tela. Na opção saldo, apenas mostrar o saldo atual na tela. Considere que um saque só pode ser realizado caso haja saldo suficiente. Criar uma variável com um valor que represente o saldo inicial.
@@ -113,37 +113,37 @@ let deposito
 
 numeroConta = Number(prompt(`Digite o número da sua conta.`))
 if (numeroConta<1000) {
-    numeroConta = (Math.random() * 621) // gera um número aleatório para o saldo.
+	numeroConta = (Math.random() * 621) // gera um número aleatório para o saldo.
 }
 
 saldo = Number((numeroConta * 7).toFixed(2))
 menuBanco = Number(prompt(`Escolha uma das opções: \n 1 - Saldo \n 2 - Depósito \n 3 - Saque.`))
 
 switch(menuBanco){
-    case 1:
-        alert(`Você escolheu a opção ${menuBanco}.\n Seu saldo é de R$${saldo}.`)
-        break
-    case 2:
-        deposito = Number(prompt(`Você escolheu a opção ${menuBanco}.\n Digite o valor a ser depositado:`))
-        saldo += Number(deposito.toFixed(2))
-        alert(`Seu novo saldo é de R$${saldo}.`)
-        break
-    case 3:
-        saque = Number(prompt(`Você escolheu a opção ${menuBanco}.\n Digite o valor a ser sacado:`))
-        if (saque>saldo) {
-        alert(`Saldo insuficiente:\n você tem R$${saldo} disponíveis.`)
-        }else{
-        saldo -= Number(saque.toFixed(2))
-        alert(`Você sacou ${saque} e o seu saldo atual é de ${saldo}.`)
-        }
-        break
-    default:
-        alert(`A opção ${menuBanco} é inválida.`)
+	case 1:
+		alert(`Você escolheu a opção ${menuBanco}.\n Seu saldo é de R$${saldo}.`)
+		break
+	case 2:
+		deposito = Number(prompt(`Você escolheu a opção ${menuBanco}.\n Digite o valor a ser depositado:`))
+		saldo += Number(deposito.toFixed(2))
+		alert(`Seu novo saldo é de R$${saldo}.`)
+		break
+	case 3:
+		saque = Number(prompt(`Você escolheu a opção ${menuBanco}.\n Digite o valor a ser sacado:`))
+		if (saque>saldo) {
+		alert(`Saldo insuficiente:\n você tem R$${saldo} disponíveis.`)
+		}else{
+		saldo -= Number(saque.toFixed(2))
+		alert(`Você sacou ${saque} e o seu saldo atual é de ${saldo}.`)
+		}
+		break
+	default:
+		alert(`A opção ${menuBanco} é inválida.`)
 }
 
 
 // 5) Em um determinado e-commerce, o frete para produtos possui o valor fixo de R$12,50. A loja possui benefícios para assinantes em três categorias: 1) Assinante Premium, ganha 20% de desconto e frete grátis 2) Assinante Gold, ganha 20% de desconto mas paga frete 3) Assinante Silver, ganha 10% de desconto mas paga frete. 4) Não assinante, sem benefícios. Faça um programa que solicite o valor da compra e a categoria de assinante (1, 2, 3 ou 4). Mostrar na tela o valor da compra de acordo com a opção escolhida.
-let compra
+let compra // valor compra
 let tipoAss // tipo de assinatura
 let frete
 
@@ -152,26 +152,27 @@ compra = Number(prompt(`Digite o valor da sua compra. Utilize ponto '.'\n e não
 tipoass = Number(prompt(`Escolha uma das opções: \n 1 - Premium \n 2 - Gold \n 3 - Silver \n 4 - Não assinante.`))
 
 switch(tipoass){
-    case 1:
-        compra = compra*0.8
-        tipoAss = "Você é assinante Premium: ganha 20% de desconto e frete grátis!"
-        break
-    case 2:
-        compra = compra*0.8+frete
-        tipoAss = "Você é assinante Gold: ganha 20% de desconto!"
-        break
-    case 3:
-        compra = compra*0.9+frete
-        tipoAss = "Você é assinante Silver: ganha 10% de desconto!"
-        break
-    case 4:
-        compra += frete
-        tipoAss = "Você ainda não é nosso assinante. Quem sabe na próxima compra você aproveita os benefícios do nosso programa."
-        break
-    default:
-        alert(`Você escolheu uma opção inválida.`)
+	case 1:
+		compra = compra*0.8
+		tipoAss = "Você é assinante Premium: ganha 20% de desconto e frete grátis!"
+		break
+	case 2:
+		compra = compra*0.8+frete
+		tipoAss = "Você é assinante Gold: ganha 20% de desconto!"
+		break
+	case 3:
+		compra = compra*0.9+frete
+		tipoAss = "Você é assinante Silver: ganha 10% de desconto!"
+		break
+	case 4:
+		compra += frete
+		tipoAss = "Você ainda não é nosso assinante. Quem sabe na próxima compra você aproveita os benefícios do nosso programa."
+		break
+	default:
+		alert(`Você escolheu uma opção inválida.`)
 }
 alert(`${tipoAss} O valor total é de R$${compra}.`)
 
 // 6) Numa competição de arremesso de peteca, o competidor tem direito a 3 arremessos para que a peteca caia em um alvo com áreas e pontuações de 0 a 5, sendo 5 no centro e 0 fora do alvo. Faça um programa que pergunte a pontuação de cada arremesso e ao final mostre o resultado (soma dos pontos) e a classifição: 15 pontos (deus da peteca), de 14 a 10 (petequeiro profissa), de 9 a 5 (petequeiro de final de semana), de 4 a 1 (pseudo-petequeiro) e 0 pontos (nunca petequeiro).
 
+let pontuacao
--- Aula 9 - 2023-08-24/Atividade - Estrut. de repetição (while e do while).js
+++ Aula 9 - 2023-08-24/Atividade - Estrut. de repetição (while e do while).js
@@ -13,19 +13,19 @@ txB = 0.015
 // txA > txB
 
 while(popA < popB) {
-    popA = popA * (1+txA)
-    popB = popB * (1+txB)
-    if (popA<popB) {
-    i++ //soma 1
-    }else if (popA = popB){
-        popA = popA.toFixed(0)
-        popB = popB.toFixed(0)
-    console.log(`A população do País A igualou a do País B após ${i} anos.\n São ${popB} habitantes em cada País.`)    
-    }else{
-        popA = popA.toFixed(0)
-        popB = popB.toFixed(0)
-    console.log(`A população do País A superou a do País B após ${i} anos.\n São espectivamente ${popA} e ${popB} habitantes.`)
-    }
+	popA = popA * (1+txA)
+	popB = popB * (1+txB)
+	if (popA<popB) {
+	i++ //soma 1
+	}else if (popA = popB){
+		popA = popA.toFixed(0)
+		popB = popB.toFixed(0)
+	console.log(`A população do País A igualou a do País B após ${i} anos.\n São ${popB} habitantes em cada País.`)    
+	}else{
+		popA = popA.toFixed(0)
+		popB = popB.toFixed(0)
+	console.log(`A população do País A superou a do País B após ${i} anos.\n São espectivamente ${popA} e ${popB} habitantes.`)
+	}
 }
 
 
@@ -36,17 +36,17 @@ let i = 3
 // alert(`Insira números para verificar se são primos.`)
 numUm = Number(prompt(`Insira o primeiro número a ser verificado:`))
 if (numUm == 2){
-    alert(`${numUm} é primo.`)
+	alert(`${numUm} é primo.`)
 }else if(numUm%2 == 0){
-    alert(`${numUm} não é primo.`)
+	alert(`${numUm} não é primo.`)
 }else{
 while(numUm>i) {
-    if(numUm%i == 0){
-    alert(`Não é Primo.`)
-    break
-    }else{
-    i = i+2 //soma 2 para só pegar os números ímpares
-    }}
+	if(numUm%i == 0){
+	alert(`Não é Primo.`)
+	break
+	}else{
+	i = i+2 //soma 2 para só pegar os números ímpares
+	}}
 if(i == numUm){
 console.log(`${numUm} é primo.`)
 }else{
@@ -57,20 +57,21 @@ console.log(`${numUm} não é primo.`)
 let numeroFinal = prompt(`Digite um número para verificação.`)
 let numeroAtual = 2
 while (numeroAtual <= numeroFinal){
-        let ehPrimo = true;
-        let divisor = 2;
-do {
-    //verificação
-        if(numeroAtual % divisor ===o){
-            ehPrimo = false;
-            break;
-        }
-        divisor++
-} while (divisor <= numeroAtual / 2)
-if(ehPrimo){
-    alert('Esses números são os primos: ${numeroAtual')
-}
-numeroAtual++
+		let ehPrimo = true;
+		let divisor = 2;
+	do {
+	//verificação
+		if(numeroAtual % divisor === 0){
+			ehPrimo = false;
+			break;
+		}
+		divisor++
+	} while (divisor <= numeroAtual / 2)
+		if(ehPrimo){
+			alert(`Esses números são os primos: ${numeroAtual}`)
+		}
+   numeroAtual++
+   console.log(`${numeroAtual}`)
 }
 
 
@@ -91,6 +92,45 @@ while (num%i != 0) {
 // d) A média de preço dos vinis.
 // e) O vinil mais caro e o mais barato.
 
+//variáveis do vinil
+let dinheiroDisponivel = 200;
+let totalGasto = 0;
+let quantidadeVinis = 0;
+let vinilMaisCaro = 0;
+let vinilMaisBarato = 0;
+// Fazer um programa que peça o nome e o valor do vinil 5x.
+do {
+  let nomeVinil = prompt("Digite o nome do vinil:");
+  let precoVinil = parseFloat(prompt("Digite o preço do vinil:"));
+// verificação: se o valor do vinil é menor que 200 (dinheiro que tenho).
+	if (precoVinil > dinheiroDisponivel) {
+		alert("Valor do vinil é maior que o dinheiro disponível. Compra encerrada.");
+	 break;
+	}
+// Fazer com que o programa também crie a informação de quanto dinheiro eu gastei, além da quantidade de discos comprados.
+  totalGasto += precoVinil;
+  quantidadeVinis++;
+// Verificação: qual vinil é o mais caro e qual o mais barato.
+	if (precoVinil > vinilMaisCaro) {
+	vinilMaisCaro = precoVinil;
+	}
+	if (precoVinil < vinilMaisBarato) {
+	vinilMaisBarato = precoVinil;
+	}
+// Criar uma condição de parada do loop: se meu dinheiro acabar o programa se encerra. Para isso há a variável que reduz o custo do disco a partir dos 200 iniciais.
+  dinheiroDisponivel -= precoVinil;
+} while (dinheiroDisponivel > 0);   //condição para parar o programa
+	let mediaPrecoVinil = totalGasto / quantidadeVinis;  //media do preço dos discos comprados
+	let dinheiroSobrou = 200 - totalGasto; // dinheiro que sobrou.
+
+//informações na tela
+alert(`Total gasto: R$${totalGasto.toFixed(2)}
+Dinheiro que sobrou: R$${dinheiroSobrou.toFixed(2)}
+Quantidade de vinis comprados: ${quantidadeVinis}
+Média de preço dos vinis: R$${mediaPrecoVinil.toFixed(2)}
+Vinil mais caro: R$${vinilMaisCaro.toFixed(2)}
+Vinil mais barato: R$${vinilMaisBarato.toFixed(2)}`);
+
 // 5) Santa Catarina é conhecida por suas quatro estações bem definidas, o que o torna um estado com alta amplitude térmica (diferença entre a maior e a menor temperatura, registrada em um período). O INMET, Instituto Nacional de Meteorologia, pretende ter os dados de amplitude térmica de uma cidade ou estado. Desenvolva um programa que calcule e mostre os dados diários (segunda à sexta) e a média de amplitude térmica semanal, mostrando os resultados ao final. Exemplo: Digitar o nome da cidade ou estado, perguntar para os cinco dias da semana a maior temperatura do dia e a menor. Mostrar as amplitudes de cada dia e a média.
 
 // variáveis: 
@@ -100,13 +140,13 @@ let ampTerm = 0; // amplitude térmica
 let mediaAmpTerm = 0
 
 do {
-    let maiorTemp = Number(prompt(`Qual a maior temperatura do dia?`))
-    let menorTemp = Number(prompt(`Qual a menor temperatura do dia?`))
-    ampTerm = maiorTemp - menorTemp;
-    alert(`A amplitude térmica de hoje é ${ampTerm}.`)
-    mediaAmpTerm += ampTerm
+	let maiorTemp = Number(prompt(`Qual a maior temperatura do dia?`))
+	let menorTemp = Number(prompt(`Qual a menor temperatura do dia?`))
+	ampTerm = maiorTemp - menorTemp;
+	alert(`A amplitude térmica de hoje é ${ampTerm}.`)
+	mediaAmpTerm += ampTerm
 
-    diaSemana++
+	diaSemana++
 }while (diaSemana<5)
 
 mediaAmpTerm = mediaAmpTerm/5
@@ -114,7 +154,38 @@ console.log(`A média semanal da amplitude térmica é de ${mediaAmpTerm}`)
 
 
 // 6) SCRUM é uma metodologia ágil de gerenciamento de projetos muito utilizada na área da tecnologia. Dentro do SCRUM, o planejamento é realizado através de 'sprints' que normalmente variam de 15 a 30 dias. Criar um programa onde o usuário deve digitar o tempo total do projeto em dias e o período das sprints. Após calculado o número de sprints (média), arredondando o número para baixo, cadastrar para cada sprint um título e um objetivo.
+// um programa que precise passar o tempo total do projeto em dias 
+// pedir também o número de sprints 
+// calcular media de sprints , usar math round
+// ainda pedir um titulo e um objetivo após o calculo da media e exibir tudo 
+// Solicitação das informações da sprint:
+let tempoTotalProjeto = Number(prompt ("Digite o número total do projeto em dias: "));
+let periodoSprint = Number(prompt("Digite o período de tempo de cada sprint: "))
 
+let mediaSprint = Math.floor( tempoTotalProjeto / periodoSprint); //calculamos a media, usei o Math.floor que ele arredonda números para baixo.
+
+
+alert(`Teremos ${mediaSprint} sprint(s)`)
+
+//crio um contador pois temos que incrementar para cada sprint
+let contador = 0;
+
+//condição de parada. Iremos pedir o titulo e o objetivo apenas para as sprints existentes
+while (contador < mediaSprint) {
+
+let tituloProjeto = prompt("Cadastre o titulo do projeto:")
+let objetivoProjeto = prompt("Cadastre o objetivo do projeto:")
+
+contador ++ //incrementamos o contador
+
+//passamos as informações na tela
+alert(`O tempo total do projeto foi estimado em ${tempoTotalProjeto} dias
+Cada sprint terá ${periodoSprint} dias
+Teremos na sprint ${contador}:
+	O título = ${tituloProjeto}
+	O objetivo = ${objetivoProjeto} `)
+
+}
 // 7) Em jogos de tabuleiro do gênero RPG, um dos dados que pode ser utilizado é o dado D20. Esse dado possui 20 faces ou "lados" com valores de 1 a 20. Criar um sistema simples de jogo, onde no início é solicitado o número de jogadores e para cada jogador deve-se armazenar valores de quatro jogadas de um D20. A pontuação do jogador é definida excluindo o primeiro e o último valor, somando então os valores da segunda e terceira jogadas. Ao final mostrar a soma de cada jogador e suas posições. (DESAFIO: validar os valores de 1 a 20, ou seja, caso um valor digitado não esteja entre dentro dos valores do dado, repetir a pergunta)
 
 // utilizou a ideia de " jogador+i "
@@ -124,23 +195,23 @@ let i = 0
 numeroJogadores = Number(prompt("Digite o número de jogadores:"))
 
 while(i<numeroJogadores) {
-    let jogadorNome = "Jogador "+(1+i); // Aqui foi o pulo do gato: inserir o i dentro de uma variável declarada dentro do while. É um while dentro do outro.
-    let somaPontuação = 0;
-    let quantidadeDeJogadas = 0;
-    
-    while (quantidadeDeJogadas < 4){ // porque são 4 jogadas
-        let dado = Number(prompt(`Insira o valor obtido no dado para o ${jogadorNome}.`));
-    
-        while(dado > 20 || dado < 0 ) {
-            dado = Number(prompt(`Valor inválido. Insira novamente o valor obtido no dado para o ${jogadorNome}.`))
-        }
-        quantidadeDeJogadas++
-        if (quantidadeDeJogadas === 2 || quantidadeDeJogadas === 3) {
-            somaPontuação += dado;
-        }
-    }
-    console.log(`${jogadorNome} - Pontuação: ${somaPontuação}.`);
-    i++
+	let jogadorNome = "Jogador "+(1+i); // Aqui foi o pulo do gato: inserir o i dentro de uma variável declarada dentro do while. É um while dentro do outro.
+	let somaPontuação = 0;
+	let quantidadeDeJogadas = 0;
+	
+	while (quantidadeDeJogadas < 4){ // porque são 4 jogadas
+		let dado = Number(prompt(`Insira o valor obtido no dado para o ${jogadorNome}.`));
+	
+		while(dado > 20 || dado < 0 ) {
+			dado = Number(prompt(`Valor inválido. Insira novamente o valor obtido no dado para o ${jogadorNome}.`))
+		}
+		quantidadeDeJogadas++
+		if (quantidadeDeJogadas === 2 || quantidadeDeJogadas === 3) {
+			somaPontuação += dado;
+		}
+	}
+	console.log(`${jogadorNome} - Pontuação: ${somaPontuação}.`);
+	i++
 }
 
 // 8) Na maioria das linguagens de programação, é possível contar caracteres de uma string (palavra, frase, etc). Criar um sistema no qual o usuário digite 5 palavras e ao final mostre o tamanho em número de caracteres, de cada uma delas. (Pesquisar sobre o comando length, para solução).
@@ -152,10 +223,10 @@ let palavra
 let novaPalavra
 alert(`Digite 5 palavras:`)
 while (i<6){
-    palavra = "palavra "+i
-    novaPalavra = prompt(`Digite a ${palavra}:`)
-    i++
-    lista = `${lista}'${novaPalavra}' tem ${novaPalavra.length} caracteres. `
+	palavra = "palavra "+i
+	novaPalavra = prompt(`Digite a ${palavra}:`)
+	i++
+	lista = `${lista}'${novaPalavra}' tem ${novaPalavra.length} caracteres. `
 }
 
 // 9) Um determinado restaurante trabalha com limite de lotação de 50 pessoas. Porém, mesmo com esse limite, o restaurante pode encerrar seu atendimento antes do horário por falta de insumos para produção das refeições. Criar um programa que receba os dados da quantidade de pessoas que chegam ao restaurante (repetidamente) e se os insumos estão "ok" ou "insuficientes". Caso alguma das duas possibilidades aconteça, encerrar o programa e mostrar ao final o motivo do encerramento (atingiu limite de pessoa ou faltaram insumos).
