c3831751fb242dbc44e6fc97570ad34e0f7455b9
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-09-12 13:06:26 -0300

    Atualização 13:06

--- 2023-09-06 - Aula 16/Atividades Funções.js
+++ 2023-09-06 - Aula 16/Atividades Funções.js
@@ -1,10 +1,16 @@
-/* Exercícios:
-1- Crie uma função anônima para gerar uma mensagem de convite de casamento, que deve receber nomeConvidado, data, horário, local e nomeNoivos. E, a partir disso, monte uma mensagem convidando a pessoa para o casamento. Realize pelo menos 3 chamadas para ela e valide se o retorno foi o esperado. */
-
-/* let desligar = function(parâmetros){
+/* exemplo de função:
+let desligar = function(parâmetros){
     //corpo da função
 }
-*/
+
+Atividade (Funções)
+
+1- Crie uma função anônima para gerar uma mensagem de convite de casamento, que
+deve receber nomeConvidado, data, horário, local e nomeNoivos e a partir disso
+monte uma mensagem convidando a pessoa para o casamento. Realize pelo menos 3
+chamadas para ela e valide se o retorno foi o esperado. */ 
+
+
 
 let mensagemCasorio = function(nomeConvidado, data, horario, local, nomeNoivos){
     return (`Olá ${nomeConvidado}! Nós, ${nomeNoivos}, convidamos você a participar da nossa cerimônia de casamento no dia ${data}, às ${horario}. Local: ${local}.`)
@@ -12,3 +18,21 @@ let mensagemCasorio = function(nomeConvidado, data, horario, local, nomeNoivos){
 
 let mensagem = mensagemCasorio ('Rafa', '18 de agosto', '13:14', 'Bistek', 'Adevir e Zeocleide')
 console.log(mensagem)
+
+/* 
+2 - Crie uma calculadora de média que solicite ao usuário um conjunto de números e
+calcule a média deles. Use uma função autoinvocada para encapsular o código da
+calculadora.
+*/
+
+
+
+
+
+
+
+
+/* 
+3 - Crie um Array numérico com 10 elementos e a partir dele utilize os métodos
+mostrados para verificar se todos são ímpares. Além disse crie outro Array a partir
+dele onde os novos elementos devem ser os antigos ao quadrado. */
---
+++ 2023-09-11 - Aula 17/Atividades aula.js
@@ -0,0 +1,35 @@
+/* Faltei esta aula pois estávamos no aeroporto em SP. O professor realizou a avaliação 2 (0,2 da nota final) e lançou a avaliação 3 (0,6 da nota final).
+*/
+
+
+    
+/* >>>>>>>>>>> Avaliação III (Prática) <<<<<<<<<<
+
+##### Desenvolvimento de CRUD de Produto #####
+
+CRUD é um acrônimo para Creat, Read, Update, Delete. Quando falamos desta "sigla" na programação, pensamos diretamente na combinação dessas funções (criar, ler, atualizar e deletar) com dados estruturados, seja um banco de dados, localStorage ou mesmo um vetor ou lista local.
+
+Trazendo para para abordagem mais simplista, um CRUD bem programado deve permitir ao usuário:
+
+-> criar (cadastrar, adicionar, gravar, salvar) um dado
+-> ler (ver, acessar, listar, visualizar) um dado ou a lista de dados
+-> atualizar (alterar, editar, modificar) um dado
+-> deletar (apagar, excluir) um dado
+
+A atividade "Desenvolvimento de CRUD de Produto" irá avaliar a programação de um CRUD no qual o usuário possa criar, ler, atualizar e deletar produtos e suas propriedades. As propriedades (informações) de cada produto são: marca, nome e valor. É aconselhável uso de métodos de vetores para manipulação dos dados e implementação do CRUD e funções para organização do código.
+
+
+((((( Critérios de Avaliação )))))
+
+(1,0) Criar uma mensagem de boas-vindas explicando o sistema.
+(1,0) Criar menu funcional (com loop) com as opções do CRUD e a opção "sair".
+(1,0) Implementar a opção "cadastrar produto".
+(1,0) Implementar a opção "deletar produto".
+(1,0) Implementar a opção "editar produto".
+(1,0) Implementar a opção "listar produtos".
+(1,0) Mostrar a lista organizada com as marcas, nomes e valores de todos produtos
+(1,0) Criar mensagens de feedback para o usuário após as ações.
+(1,0) Validar para não permitir cadastro de um produto com mesmo nome.
+(1,0) Validar para não permitir valores negativos ou zero.
+ */
+
