e7965ac853ecc3c511699dbd8f8fd2ca2569d6f9
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-09-13 20:54:11 -0300

    atualização 20:54

--- 2023-09-11 - Aula 17 (faltei)/Prova 3 (fiz em sala) .js
+++ 2023-09-11 - Aula 17 (faltei)/Prova 3 (fiz em sala) .js
@@ -19,14 +19,18 @@ Nos itens 3 a 6, onde pede para implementar opções, provavelmente terei que cr
 No item 10, tem que colocar um loop caso o usuário digite um valor não permitido com uma opção de 'abortar missão' após o primeiro erro.
 */
 
+// listaProdutos = ['MARCA', 'NOME DO PRODUTO', 'VALOR']
+listaProdutos = [[`KICHUTE`, `SAPATO`, 16.99],[`PLASTIQUARE`, `POTE`, 1.50],[`JAYUNG`, `PESO DE PORTA`, 66.66]]
+//console.table(listaProdutos)
 
-
-alert(`Bem vindo ao CRUD v1.0 do programa Entra21. Você pode realizar diversas funções no banco de dados, que contém as informações marca, nome do produto e valor.`)
+//alert(`Bem vindo ao CRUD v1.0 do programa Entra21. Você pode realizar diversas funções no banco de dados, que contém as informações marca, nome do produto e valor.`)
 let A = `cadastrar produto`
 let B = `deletar produto`
 let C = `editar produto`
 let D = `listar produtos`
 let opcaoMenu
+
+
 
 do{
 	opcaoMenu = prompt(`Digite uma das opções: \n A- ${A} \n B- ${B} \n C- ${C} \n D- ${D} \n 0- sair`).toUpperCase()
@@ -40,6 +44,16 @@ do{
 		case 'A':
 			alert(`Você escolheu a opção ${opcaoMenu}: ${A}.`)
 			//abrir função para cadastrar o produto. Penso que pode ser uma função que pergunta as 3 características e faz um push.
+			let marca = prompt(`Digite a marca do produto:`).toUpperCase()
+			let nomeDoProduto = prompt(`Digite o nome do novo produto:`).toUpperCase()
+			let valor = Number(prompt(`Digite o valor do produto, utilizando ponto '.' e 2 casas decimais (ex: 16.39):`)).toFixed(2)
+			valor = Number(valor)
+			function cd(marca, nomeDoProduto, valor) {
+    			let novoProduto = [`${marca}`, `${nomeDoProduto}`, valor]
+				listaProdutos.push(novoProduto)
+			}
+			cd(marca, nomeDoProduto, valor)
+			console.table(listaProdutos)
 			break
 		
 		case 'B':
@@ -54,7 +68,7 @@ do{
 
 		case 'D':
 			alert(`Você escolheu a opção ${opcaoMenu}: ${D}.`)
-			// abre a função de listar produtos, que será usada novamente nos casos B e C
+			// abre a função de listar produtos, que será usada novamente nos casos B e C. A questão é como organizar essa lista.
 			break
 
 		default:
