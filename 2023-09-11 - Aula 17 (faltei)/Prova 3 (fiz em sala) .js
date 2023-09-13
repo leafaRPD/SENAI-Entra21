428d5cfaf28443ef3cdee8d2f593b36b97f06d46
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-09-13 18:27:18 -0300

    Atualização das 18:27

--- 2023-09-11 - Aula 17 (faltei)/Prova 3 (fiz em sala) .js
+++ 2023-09-11 - Aula 17 (faltei)/Prova 3 (fiz em sala) .js
@@ -1,5 +1,4 @@
-/*
-(1,0) Criar uma mensagem de boas-vindas explicando o sistema.
+/*(1,0) Criar uma mensagem de boas-vindas explicando o sistema.
 (1,0) Criar menu funcional (com loop) com as opções do CRUD e a opção "sair".
 (1,0) Implementar a opção "cadastrar produto".
 (1,0) Implementar a opção "deletar produto".
@@ -11,43 +10,49 @@
 (1,0) Validar para não permitir valores negativos ou zero.
 
 marca, nome e valor
-*/
 
-/*
 listaMarcas -> coluna 0; terá valores repetidos caso uma marca tenha mais de 1 produto
 listaProduto -> coluna 1; terá o nome do produto. Só pode se repetir se mais de uma marcar produzir o mesmo produto.
 listaPreço   -> coluna 2: porerá ter qualquer valor numérico, com 2 casas decimais.
-*/ 
 
+Nos itens 3 a 6, onde pede para implementar opções, provavelmente terei que criar 1 função para da opção.
+No item 10, tem que colocar um loop caso o usuário digite um valor não permitido com uma opção de 'abortar missão' após o primeiro erro.
+*/
 
-/* para criar o menu, usar switch case, conforme exemplo:
 let opcaoMenu
+do{
+	opcaoMenu = prompt(`Digite uma das opções: \n A- cadastrar produto \n B- deletar produto \n C- editar produto \n D- listar produtos \n 0- sair`).toUpperCase()
 
-opcaoMenu = prompt(`Digite uma das opções: \n A- Falar com a atendente \n B- Falar com o RH \n C- Falar com o gerente \n D- Sair`).toUpperCase()
+	switch(opcaoMenu){
+		case '0':
 
-switch(opcaoMenu){
-	case 'A':
+			alert(`Você escolheu a opção ${opcaoMenu}: Sair.`)
+			break
 
-		alert(`Você escolheu a opção ${opcaoMenu}: Falar com a atendente.`)
-		break
+		case 'A':
 
-	case 'B':
+			alert(`Você escolheu a opção ${opcaoMenu}: cadastrar produto.`)
+			break
+		
+		case 'B':
 
-		alert(`Você escolheu a opção ${opcaoMenu}: Falar com o RH.`)
-		break
+			alert(`Você escolheu a opção ${opcaoMenu}: Falar com o RH.`)
+			break
 
-	case 'C':
+		case 'C':
 
-		alert(`Você escolheu a opção ${opcaoMenu}: Falar com o gerente.`)
-		break
-	
-	case 'D':
+			alert(`Você escolheu a opção ${opcaoMenu}: Falar com o gerente.`)
+			break
 
-		alert(`Você escolheu a opção ${opcaoMenu}: Sair.`)
-		break
+		case 'D':
 
-	default:
+			alert(`Você escolheu a opção ${opcaoMenu}: Sair.`)
+			break
 
-		alert(`A opção ${opcaoMenu} é inválida.`)
+		default:
+			opcaoMenu = 'Z'
+			alert(`A opção escolhida é inválida.`)
 
-}*/
+	}
+} while (opcaoMenu == 'Z')
+
