50b5757722e02ebd8a663bf494b62b0b7009f704
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-09-15 18:58:21 -0300

    atualização 18:58

---
+++ 2023-09-15 - Aula 21/Anotações aula.js
@@ -0,0 +1,6 @@
+/* ontem e hj, dias 14 e 15/9, foram aulas com o Rafael Ventura.
+Na pasta do dia 15 o professor enviou os arquivos feitos na aula do dia 15:
+'Lógico_1.brM3' é para ser aberto no brModelo;
+'fisico_clube-do-livro.architect' abrir no SQL POWER ARCHITECT;
+
+*/
