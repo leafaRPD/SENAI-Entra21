a1df48eeccf3e1bb5f21eaf30fc52ccdf25129ff
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-09-26 20:58:39 -0300

    Atualização 20:58

---
+++ 2023-09-26 - Aula 28/Anotações aula.js
@@ -0,0 +1,4 @@
+/* hoje continuamos com aula do Lucas, com os arquivos de ontem. Editei no próprio arquivo da aula de ontem e fizemos isso até o intervalo.
+Após o intervalo, houve apresentação de slides.
+
+*/
