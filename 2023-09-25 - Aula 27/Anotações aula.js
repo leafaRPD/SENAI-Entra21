9f1f13ee04eddc9c04b0f5ac5d4c714992cfafbd
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-09-25 21:38:59 -0300

    Atualização 21:38

--- 2023-09-25 - Aula 27/Anotações aula.js
+++ 2023-09-25 - Aula 27/Anotações aula.js
@@ -1,2 +1,8 @@
 // Aula 26 de sexta dia 22/9 foi de inglês com o Adrian. Fiz a prova de validação e fui embora.
 // Hoje, dia 25/9, aula do Lucas.
+
+/*site interessantes de dicas  
+https://css-tricks.com/
+https://flukeout.github.io/
+
+*/
---
+++ 2023-09-25 - Aula 27/modal.js
@@ -0,0 +1,14 @@
+// arquivo para fazer rodar o arquivo dialog.html.
+let elementoModal = document.getElementById('modal')
+
+function ModalOpen() {
+    elementoModal.showModal()
+
+}
+
+
+
+function ModalClose() {
+    elementoModal.close()
+    
+}
