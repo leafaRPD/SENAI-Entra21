6494b7bd8dabe2a84158a8ea3f0b284af242b5f5
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-10-02 20:53:24 -0300

    Atualização 20:53

---
+++ 2023-10-02 - Aula 30/Anotações aula.js
@@ -0,0 +1,14 @@
+/* dia 28/09 foi prova do Rafael Ventura.
+hoje, aula do Lucas. Amanhã, prova de css.
+
+Eu deveria ter feito o exercício de copiar os sites; haviam 3 modelos para exercitar (aula do dia 25/09) */
+/* dicas de CSS: joguinhos e sites de desfios:
+flexboxfroggy.com
+cssbattle.dev
+flexboxdefense.com
+cssgridgarden.com
+css grid layout (aula da Origamid) -> https://www.youtube.com/watch?v=hKXOVD2Yrj8
+
+//                    --  Grid layout
+
+
