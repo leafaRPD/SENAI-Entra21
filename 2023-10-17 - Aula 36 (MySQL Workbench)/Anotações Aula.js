afd055ce4db98353a5632efcb521154125ad3dab
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-10-17 21:44:30 -0300

    Atualização 21:44

--- 2023-10-17 - Aula 36 (MySQL Workbench)/Anotações Aula.js
+++ 2023-10-17 - Aula 36 (MySQL Workbench)/Anotações Aula.js
@@ -27,12 +27,43 @@ show grants for 'usuario_normal'@'LOCALHOST';
 /* Após esses códigos, continuamos nos exeercícios sobre o clube do livro:
 group by (sum, max, min, avg, count)
 
-1 - Qual a maior venda do clube do livro?
+1 - Qual a maior venda do clube do livro? (não deu certo)
+select max(qtd_vendida) as total_vendas
+from vendas;
+
+
 2 - Qual o vendedor que mais vendeu?
+select id_vendedor, sum(qtd_vendida) as soma_vendas
+from vendas
+group by id_vendedor
+order by soma_vendas desc
+limit 1;
+
 3 - A quantidade de livros que cada vendedor vendeu?
+select vendas.id_vendedor, vendedores.nome_vendedor, sum(vendas.qtd_vendida) as soma_vendas
+from vendas
+inner join vendedores
+on vendas.id_vendedor = vendedores.id_vendedor
+group by id_vendedor
+order by soma_vendas desc;
+
 4 - Qual o livro mais vendido?
+select livros.id_livro, livros.nome_livro, sum(vendas.qtd_vendida) as soma_livros
+from vendas as v // alias
+inner join livros as l // alias. Como foi definido, na linha abaixo tem que usar o alias e não o nome inteiro, senão dá erro.
+on v.id_livro = l.id_livro
+group by id_livro
+order by soma_livros desc;
+
 5 - Quais livros não foram vendidos?
-6 - Existe algum livro que teve vendas e não faz mais parte da lista de livros comercializados (null em 'IDdolivro')
+select livros.id_livro, nome_livro, max(qtd_vendida)
+from livros
+left join vendas
+on livros.id_livro = vendas.id_livro
+group by livros.id_livro;
+
+6 - Existe algum livro que teve vendas e não faz mais parte da lista de livros comercializados (null em 'id_livro') // não consegui fazer
+
 7 - Qual a média de vendas entre os vendedores?
 8 - Qual foi o total de todas as vendas feitas?
 9 - Qual o maior pedido em termos de valor?
