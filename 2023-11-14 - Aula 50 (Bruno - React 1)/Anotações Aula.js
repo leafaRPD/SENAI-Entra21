9663075e6c51466633484d0c1c7e48f699c6fa96
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-11-14 19:14:57 -0300

    Atualização 20:00

---
+++ 2023-11-14 - Aula 50 (Bruno - React 1)/Anotações Aula.js
@@ -0,0 +1,7 @@
+/* Faltei a semana passada inteira (6 a 10), e dia 13/11 o prof. cancelou a aula pq estava doente.
+Hoje, dia 14/11: React aula 1 -> primeira aula com o prof Bruno. 
+ORM, sigla para Object Relational Mapper -> o mercado usa o 'Prisma'
+Sugestão de livro: Orientação a Objetos e SOLID para ninjas
+
+
+*/
