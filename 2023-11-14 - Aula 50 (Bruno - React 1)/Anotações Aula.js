d2837e6a15d7fc553afbf6e40445f453f776664e
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-11-14 21:52:15 -0300

    Atualização 21:53

--- 2023-11-14 - Aula 50 (Bruno - React 1)/Anotações Aula.js
+++ 2023-11-14 - Aula 50 (Bruno - React 1)/Anotações Aula.js
@@ -3,5 +3,27 @@ Hoje, dia 14/11: React aula 1 -> primeira aula com o prof Bruno.
 ORM, sigla para Object Relational Mapper -> o mercado usa o 'Prisma'
 Sugestão de livro: Orientação a Objetos e SOLID para ninjas
 
+Fizemos um novo repositório no Git, para teste. Relembrando os comandos:
+'git bash here' na pasta do Windows em que se deseja gravar as coisas para depois mandar para o git.
+
+git clone <link gerado pelo GitHub, dentro de 'code'>
+
+git add . 
+
+git commit -m "mensagem do usuario (como 'teste de commit')"
+
+git push
+
+
+// feito isso, como criar novo projeto em React?
+
+npm create vite@latest
+
+Escolhemos um nome (FundamentosReact) e demos enter;
+Depois tinha que digitar um novo nome; ele já faz uma sugestão e demos enter;
+Após, escolhemos 'React' e depois 'JavaScript'.
+
+Inserir o comando 'npm i' para instalar um pacote de coisas na máquina.
+
 
 */
