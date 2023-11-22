22ffd7b7e907905ce09bc872866f5ab739777247
leafaRPD <109984481+leafaRPD@users.noreply.github.com>
2023-11-22 18:48:09 -0300

    Atualização

---
+++ 2023-11-22 - Aula 53 (Ruan - Aula 3)/Anotações Aula.js
@@ -0,0 +1,17 @@
+/* para iniciar um projeto em Node, rodar o comando 'npm init' no terminal.
+
+criar pasta 'app',
+'cd app' no terminal para mudar para a nova pasta 'app'
+'npm init' no terminal.
+
+
+'npm start'
+
+criando o script chamado 'start' (no package.json):
+"start": "node index.js",
+
+agora no terminal podemos digitar 'npm start' e ele rodar o index.js automaticamente. 
+
+
+
+*/
